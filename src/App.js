42c1d925194edb0734323a21be4f17200ceabc8a
supachai <611463023@crru.ac.th>
2021-04-11 13:13:46 +0700

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useRef, useState } from "react";
+import ml5 from "ml5";
 
-function App() {
+let classifier;
+const url = "https://teachablemachine.withgoogle.com/models/RrGhF9eh_/"
+export default function App() {
+  const videoRef = useRef();
+  
+  const [result, setResult] = useState([]);
+  const [loaded, setLoaded] = useState(false);
+  const [input,setInput] = useState(0)
+  useEffect(() => {
+    startWebcam()
+
+    classifier = ml5.imageClassifier(url+"model.json", () => {
+      console.log("Load Model success")
+    });
+    console.log(classifier)
+
+
+  }, []);
+  useEffect(() => {
+    if (classifier) {
+      if(input === 0){
+        classifier.classify(videoRef.current, (error, results) => {
+          if (error) {
+            console.error(error);
+            return;
+          }
+          setResult(results);
+        }); 
+      }else if(input === 1){
+        stopWebcam()
+      }
+     
+    }
+  }, [result,input])
+  const startWebcam = () =>{
+      navigator.mediaDevices
+        .getUserMedia({ video: true })
+        .then(stream => videoRef.current.srcObject = stream)
+        .catch(console.log);
+  }
+  const stopWebcam = () =>{
+    const stream =  videoRef.current.srcObject;
+    const tracks = stream.getTracks();
+    tracks.forEach(track =>tracks.stop());
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <div style={{display: 'flex',flexDirection:'column',alignItems: 'center',justifyContent: 'center'}}>
+      <div className="flex" >
+        <button type="button" className="btn btn-outline-success" onClick={() => setInput(0)}>WebCam</button>
+        <button type="button" className="btn btn-outline-warning" onClick={() => {setInput(1);setResult([])}}>File</button>
+      </div>
+      {input === 0 ? <video
+        ref={videoRef}
+        width={window.innerWidth}
+        height={window.innerHeight*.75}
+        autoPlay
+      ></video> 
+      :input === 1 ? <input type='file' accept="image/jpeg"/>
+      : <></>}
+      
+      {classifier && result && result !== undefined ? 
+      <div >
+        <h1 >Result</h1>
+        {result.map((item, ind) => <p key={ind}>{item.label} 
 
-export default App;
+        <div className="progress">
+        <div className="progress-bar" role="progressbar" style={{width:(item.confidence*100).toFixed(2)}} aria-valuenow={(item.confidence*100).toFixed(2) } aria-valuemin="0" aria-valuemax="100">{(item.confidence*100).toFixed(2) }%</div>
+        </div>
+        </p>)}
+      </div>
+      
+      :<>Loading...</> }
+      
+    </div>
+  )
+}
